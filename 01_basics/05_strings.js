0b24312fabf4d9c27f4cb33d23b82a6f9eaa5505
akarsh286 <aakarshshukla052@gmail.com>
2025-01-14 06:02:03 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "Akarsh "
+
+const num = 90;
+
+ // console.log(name + num + " value ");
+
+ // console.log(`hello my name  is ${ name}  and my age  is ${num}  :--`);
+ 
+ // ${} :-- string intropolition;
+
+ const gamename = new String ( 'Ak-a-r-s-hsh-ukla' )
+
+ // console.log(gamename[1] );
+
+ // console.log(gamename.__proto__);
+
+ //console.log(gamename.length);
+
+
+ //console.log(gamename.toUpperCase());
+ 
+
+ // console.log(gamename.charAt(3));
+// console.log(gamename.indexOf("h"));
+
+const newstring = gamename.substring(0 , 5)
+
+console.log(newstring);
+
+const anotherstring = gamename.slice(0 , 5)
+ console.log(anotherstring);
+
+ const newstringone ="    akarsh   "
+ console.log(newstringone.trim());
+ 
+ const url = "heelo90"
+
+ console.log(url.replace('90' , '-'));
+ 
+ console.log(gamename.split('-'));
+ 
